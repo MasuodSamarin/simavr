55423cc1c4465dfdd96e9f9a720a460552dfa2e0
blakewford <blakewford@gmail.com>
2015-06-06 23:46:39 -0500

    Build natively for Android

---
+++ simavr/jni/Android.mk
@@ -0,0 +1,34 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := run_avr
+LOCAL_CFLAGS += -std=c99
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../ $(LOCAL_PATH)/../sim
+LOCAL_SRC_FILES := ../sim/run_avr.c \
+       ../sim/sim_avr.c \
+       ../sim/sim_hex.c \
+       ../sim/sim_core.c \
+       ../sim/sim_cycle_timers.c \
+       ../sim/sim_gdb.c \
+       ../sim/sim_interrupts.c \
+       ../sim/avr_adc.c \
+       ../sim/avr_bitbang.c \
+       ../sim/avr_eeprom.c \
+       ../sim/avr_extint.c \
+       ../sim/avr_flash.c \
+       ../sim/avr_ioport.c \
+       ../sim/avr_lin.c \
+       ../sim/avr_spi.c \
+       ../sim/avr_timer.c \
+       ../sim/avr_twi.c \
+       ../sim/avr_uart.c \
+       ../sim/avr_usb.c \
+       ../sim/avr_watchdog.c \
+       ../cores/sim_mega328.c \
+       ../cores/sim_mega32u4.c \
+       ../cores/sim_megax8.c \
+       ../sim/sim_io.c \
+       ../sim/sim_irq.c \
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ simavr/jni/Application.mk
@@ -0,0 +1,4 @@
+#APP_ABI := all
+APP_ABI := armeabi-v7a
+APP_CPPFLAGS += -O3 -std=c++11
+APP_STL := gnustl_static
